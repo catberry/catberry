042137f054b6003fd0a6a5c40fca35bc7f389224
Denis Rechkunov <denis.rechkunov@gmail.com>
2014-09-23 18:28:35 +0700

    Fix code style.

--- test/lib/helpers/moduleHelper.js
+++ test/lib/helpers/moduleHelper.js
@@ -1,4 +1,4 @@
-/* 
+/*
  * catberry
  *
  * Copyright (c) 2014 Denis Rechkunov and project contributors.
@@ -8,22 +8,22 @@
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
  * files (the "Software"), to deal in the Software without restriction,
- * including without limitation the rights to use, copy, modify, merge, 
- * publish, distribute, sublicense, and/or sell copies of the Software, 
- * and to permit persons to whom the Software is furnished to do so, 
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
  * subject to the following conditions:
  *
  * The above copyright notice and this permission notice shall be included
  * in all copies or substantial portions of the Software.
-
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
  * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- * 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
  * This license applies to all parts of catberry that are not externally
  * maintained libraries.
  */
@@ -82,7 +82,9 @@ describe('lib/helpers/moduleHelper', function () {
 	describe('#getMethodToInvoke', function () {
 		it('should find method in module', function () {
 			var module = {
-					someMethodToInvoke: function () { return 'hello';}
+					someMethodToInvoke: function () {
+						return 'hello';
+					}
 				},
 				name = 'method-to-invoke',
 				method = moduleHelper.getMethodToInvoke(module, 'some', name);
