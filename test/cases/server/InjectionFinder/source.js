f3ffae593c1b572422894ecf75b490cad79913d3
Denis Rechkunov <mail@pragmader.me>
2014-09-27 13:03:16 +0700

    Add tests for injection finder.

---
+++ test/cases/server/InjectionFinder/source.js
@@ -0,0 +1,91 @@
+/*
+ * catberry
+ *
+ * Copyright (c) 2014 Denis Rechkunov and project contributors.
+ *
+ * catberry's license follows:
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * This license applies to all parts of catberry that are not externally
+ * maintained libraries.
+ */
+
+'use strict';
+
+function Module($inj1, config1, $inj2, config2, config3, $inj3) {
+	for (var i = 0; i < $inj1.length; i++) {
+		var foo = function Module2($wrong1, wrong2) {
+
+		};
+
+	}
+
+	while (true) {
+		var foo2 = function ($wrong3, wrong4) {
+
+		};
+	}
+
+	do {
+		var foo3 = function ($wrong5, wrong6) {
+
+		};
+	} while (true);
+
+	var x = true || function () {
+		function Some($wrong7) {
+
+		}
+	};
+}
+
+var Module2 = function ($inj1, config1, $inj2, config2, config3, $inj3, $inj4,
+	config4) {
+	try {
+		var foo5 = function ($wrong8, wrong9) {
+
+		};
+	} catch (e) {
+
+	}
+};
+
+(function () {
+
+	var Module3 = function (config2, config3, $inj3, $inj5, config5) {
+		try {
+			var foo6 = function ($wrong10, wrong11) {
+
+			};
+		} catch (e) {
+			var foo7 = function (wrong12) {
+
+			};
+		}
+	};
+
+})();
+
+var y = !(function () {
+	function Some($wrong13) {}
+
+	return 10;
+})();
