7d30cf4e016f747aa49101b3d988e81d2a238621
Denis Rechkunov <mail@pragmader.me>
2014-09-27 18:52:27 +0700

    Add tests for promise adapters.

---
+++ test/lib/promises/fs.js
@@ -0,0 +1,60 @@
+/*
+ * catberry
+ *
+ * Copyright (c) 2014 Denis Rechkunov and project contributors.
+ *
+ * catberry's license follows:
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * This license applies to all parts of catberry that are not externally
+ * maintained libraries.
+ */
+
+'use strict';
+var assert = require('assert'),
+	fs = require('../../../lib/promises/fs');
+
+describe('lib/promises/fs', function () {
+	describe('#exists', function () {
+		it('should determine that file exists', function (done) {
+			fs.exists(__filename)
+				.then(function (isExists) {
+					assert.strictEqual(isExists, true);
+				})
+				.then(function () {
+					done();
+				}, function (reason) {
+					done(reason);
+				});
+		});
+		it('should determine that file does not exist', function (done) {
+			fs.exists(__filename + '.never')
+				.then(function (isExists) {
+					assert.strictEqual(isExists, false);
+				})
+				.then(function () {
+					done();
+				}, function (reason) {
+					done(reason);
+				});
+		});
+	});
+});
