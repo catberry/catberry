7a2c25ae53b46c74f8fb7eb5d205f4d4c43e3a44
Denis Rechkunov <denis.rechkunov@gmail.com>
2014-10-28 18:58:55 +0600

    Fix tests for URI and punycode.

--- test/lib/URI.js
+++ test/lib/URI.js
@@ -39,7 +39,7 @@ describe('lib/URI', function () {
 	describe('constructor', function () {
 		it('should properly parse and decode URI string', function () {
 			var uriString = 'scheme-%D1%81%D1%85%D0%B5%D0%BC%D0%B0:' +
-				'//authority-%D1%85%D0%BE%D1%81%D1%82:10000' +
+				'//xn--authority--nvi0bn3b:10000' +
 				'/some/path/to-%D0%BF%D1%83%D1%82%D1%8C/' +
 				'?arg-%D0%B0%D1%80%D0%B3%D1%83%D0%BC%D0%B5%D0%BD%D1%82' +
 				'=value-%D0%B7%D0%BD%D0%B0%D1%87%D0%B5%D0%BD%D0%B8%D0%B5' +
@@ -70,7 +70,7 @@ describe('lib/URI', function () {
 	describe('#toString', function () {
 		it('should properly recombine and encode URI', function () {
 			var uriString = 'scheme-%D1%81%D1%85%D0%B5%D0%BC%D0%B0:' +
-				'//authority-%D1%85%D0%BE%D1%81%D1%82:10000' +
+				'//xn--authority--nvi0bn3b:10000' +
 				'/some/path/to-%D0%BF%D1%83%D1%82%D1%8C/' +
 				'?arg-%D0%B0%D1%80%D0%B3%D1%83%D0%BC%D0%B5%D0%BD%D1%82' +
 				'=value-%D0%B7%D0%BD%D0%B0%D1%87%D0%B5%D0%BD%D0%B8%D0%B5' +
