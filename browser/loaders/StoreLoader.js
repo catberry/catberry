1a356d4630563b15b495631553be15f70a7ac0db
Denis Rechkunov <denis.rechkunov@gmail.com>
2015-02-02 13:10:57 +0600

    Implement client-side loaders.

---
+++ browser/loaders/StoreLoader.js
@@ -0,0 +1,99 @@
+/*
+ * catberry
+ *
+ * Copyright (c) 2015 Denis Rechkunov and project contributors.
+ *
+ * catberry's license follows:
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * This license applies to all parts of catberry that are not externally
+ * maintained libraries.
+ */
+
+'use strict';
+
+module.exports = StoreLoader;
+
+/**
+ * Creates instance of store loader.
+ * @param {ServiceLocator} $serviceLocator Locator to resolve stores.
+ * @constructor
+ */
+function StoreLoader($serviceLocator) {
+	this._serviceLocator = $serviceLocator;
+	this._eventBus = $serviceLocator.resolve('eventBus');
+	this._contextFactory = $serviceLocator.resolve('contextFactory');
+}
+
+/**
+ * Current context factory.
+ * @type {ContextFactory}
+ * @private
+ */
+StoreLoader.prototype._contextFactory = null;
+
+/**
+ * Current event bus.
+ * @type {EventEmitter}
+ * @private
+ */
+StoreLoader.prototype._eventBus = null;
+
+/**
+ * Current service locator.
+ * @type {ServiceLocator}
+ * @private
+ */
+StoreLoader.prototype._serviceLocator = null;
+
+/**
+ * Current set of loaded stores.
+ * @type {Object}
+ * @private
+ */
+StoreLoader.prototype._loadedStores = null;
+
+/**
+ * Loads all stores when in a browser.
+ */
+StoreLoader.prototype.load = function () {
+	var self = this,
+		stores = {};
+	this._serviceLocator.resolveAll('store')
+		.forEach(function (store) {
+			var storeContext = Object.create(self._contextFactory.createStub());
+			storeContext.name = store.name;
+			store.constructor.prototype.$context = storeContext;
+			stores[store.name] = store;
+			self._eventBus.emit('storeLoaded', stores[store.name]);
+		});
+	this._loadedStores = stores;
+	this._eventBus.emit('allStoresLoaded', stores);
+	return stores;
+};
+
+/**
+ * Gets stores map by names.
+ * @returns {Object} Map of stores by names.
+ */
+StoreLoader.prototype.getStoresByNames = function () {
+	return this._loadedStores || {};
+};
