a201fd408680b21e5d5ede485d1d38b6f92126ef
Denis Rechkunov <mail@pragmader.me>
2016-02-20 20:42:34 +0100

    Migrate lib/helpers to ES2015.

--- test/lib/helpers/errorHelper.js
+++ test/lib/helpers/errorHelper.js
@@ -1,42 +1,15 @@
-/*
- * catberry
- *
- * Copyright (c) 2014 Denis Rechkunov and project contributors.
- *
- * catberry's license follows:
- *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without restriction,
- * including without limitation the rights to use, copy, modify, merge,
- * publish, distribute, sublicense, and/or sell copies of the Software,
- * and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- * This license applies to all parts of catberry that are not externally
- * maintained libraries.
- */
-
 'use strict';
 
-var assert = require('assert'),
-	routeHelper = require('../../../lib/helpers/errorHelper');
+const assert = require('assert');
+const errorHelper = require('../../../lib/helpers/errorHelper');
 
+/* eslint prefer-arrow-callback:0 */
+/* eslint max-nested-callbacks:0 */
+/* eslint require-jsdoc:0 */
 describe('lib/helpers/errorHelper', function() {
 	describe('#prettyPrint', function() {
-		it('should return empty string if wrong error argument', function() {
-			var result = routeHelper.prettyPrint(null, null);
+		it('should return an empty string if the wrong error argument is specified', function() {
+			const result = errorHelper.prettyPrint(null, null);
 			assert.strictEqual(result, '');
 		});
 	});
--- test/lib/helpers/moduleHelper.js
+++ test/lib/helpers/moduleHelper.js
@@ -1,206 +1,148 @@
-/*
- * catberry
- *
- * Copyright (c) 2014 Denis Rechkunov and project contributors.
- *
- * catberry's license follows:
- *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without restriction,
- * including without limitation the rights to use, copy, modify, merge,
- * publish, distribute, sublicense, and/or sell copies of the Software,
- * and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- * This license applies to all parts of catberry that are not externally
- * maintained libraries.
- */
-
 'use strict';
 
-var assert = require('assert'),
-	moduleHelper = require('../../../lib/helpers/moduleHelper');
+const assert = require('assert');
+const moduleHelper = require('../../../lib/helpers/moduleHelper');
 
-global.Promise = require('promise');
-
+/* eslint prefer-arrow-callback:0 */
+/* eslint max-nested-callbacks:0 */
+/* eslint require-jsdoc:0 */
 describe('lib/helpers/moduleHelper', function() {
 	describe('#getNameForErrorTemplate', function() {
-		it('should return name with postfix', function() {
-			var templateName = moduleHelper.getNameForErrorTemplate(
-				'some'
-			);
+		it('should return a name with postfix', function() {
+			const templateName = moduleHelper.getNameForErrorTemplate('some');
 			assert.strictEqual(
-				templateName,
-				'some' + moduleHelper.COMPONENT_ERROR_TEMPLATE_POSTFIX
+				templateName, `some${moduleHelper.COMPONENT_ERROR_TEMPLATE_POSTFIX}`
 			);
 		});
-		it('should return empty string for null value', function() {
-			var templateName = moduleHelper.getNameForErrorTemplate(null);
+
+		it('should return an empty string for a null value', function() {
+			const templateName = moduleHelper.getNameForErrorTemplate(null);
 			assert.strictEqual(templateName, '');
 		});
 	});
 
 	describe('#getCamelCaseName', function() {
-		it('should convert name to camel case with prefix', function() {
-			var badName = 'awesome-module_name',
-				camelCaseName = moduleHelper.getCamelCaseName('some', badName);
+		it('should convert a name to the camel case with the prefix', function() {
+			const badName = 'awesome-module_name';
+			const camelCaseName = moduleHelper.getCamelCaseName('some', badName);
 
 			assert.strictEqual(camelCaseName, 'someAwesomeModuleName');
 		});
 
-		it('should convert name to camel without prefix', function() {
-			var badName = 'awesome-module_name',
-				camelCaseName1 = moduleHelper.getCamelCaseName(null, badName),
-				camelCaseName2 = moduleHelper.getCamelCaseName('', badName);
+		it('should convert a name to the camel case without the prefix', function() {
+			const badName = 'awesome-module_name';
+			const camelCaseName1 = moduleHelper.getCamelCaseName(null, badName);
+			const camelCaseName2 = moduleHelper.getCamelCaseName('', badName);
 
 			assert.strictEqual(camelCaseName1, 'awesomeModuleName');
 			assert.strictEqual(camelCaseName2, 'awesomeModuleName');
 		});
 
-		it('should return string with prefix if input is in camel case',
-			function() {
-				var camelCaseName = moduleHelper.getCamelCaseName(
-					'some', 'awesomeModuleName'
-				);
+		it('should return a string with the prefix if the input is in the camel case', function() {
+			const camelCaseName = moduleHelper.getCamelCaseName('some', 'awesomeModuleName');
+			assert.strictEqual(camelCaseName, 'someAwesomeModuleName');
+		});
 
-				assert.strictEqual(camelCaseName, 'someAwesomeModuleName');
-			});
+		it('should return an input string if the input is in the camel case', function() {
+			const camelCaseName = moduleHelper.getCamelCaseName(null, 'awesomeModuleName');
+			assert.strictEqual(camelCaseName, 'awesomeModuleName');
+		});
 
-		it('should return input string if input is in camel case',
-			function() {
-				var camelCaseName = moduleHelper.getCamelCaseName(
-					null, 'awesomeModuleName'
-				);
+		it('should handle separators at the end', function() {
+			const camelCaseName = moduleHelper.getCamelCaseName(null, 'awesome-module-name-');
+			assert.strictEqual(camelCaseName, 'awesomeModuleName');
+		});
 
-				assert.strictEqual(camelCaseName, 'awesomeModuleName');
-			});
-
-		it('should handle separators at the end',
-			function() {
-				var camelCaseName = moduleHelper.getCamelCaseName(
-					null, 'awesome-module-name-'
-				);
-
-				assert.strictEqual(camelCaseName, 'awesomeModuleName');
-			});
-
-		it('should return empty string if input is empty', function() {
-			var camelCaseName1 = moduleHelper.getCamelCaseName(null, null),
-				camelCaseName2 = moduleHelper.getCamelCaseName('', '');
-
+		it('should return an empty string if the input is empty', function() {
+			const camelCaseName1 = moduleHelper.getCamelCaseName(null, null);
+			const camelCaseName2 = moduleHelper.getCamelCaseName('', '');
 			assert.strictEqual(camelCaseName1, '');
 			assert.strictEqual(camelCaseName2, '');
 		});
 	});
 
 	describe('#getOriginalComponentName', function() {
-		it('should return name without prefix', function() {
-			var originalName = moduleHelper.getOriginalComponentName(
-				moduleHelper.COMPONENT_PREFIX + 'some'
+		it('should return a name without the prefix', function() {
+			const originalName = moduleHelper.getOriginalComponentName(
+				`${moduleHelper.COMPONENT_PREFIX}some`
 			);
 			assert.strictEqual(originalName, 'some');
 		});
-		it('should return empty string for null value', function() {
-			var originalName = moduleHelper.getOriginalComponentName(null);
+
+		it('should return an empty string for the null value', function() {
+			const originalName = moduleHelper.getOriginalComponentName(null);
 			assert.strictEqual(originalName, '');
 		});
 	});
 
 	describe('#getTagNameForComponentName', function() {
-		it('should return name with prefix', function() {
-			var tagName = moduleHelper.getTagNameForComponentName(
-				'some'
-			);
+		it('should return a name with the prefix', function() {
+			const tagName = moduleHelper.getTagNameForComponentName('some');
 			assert.strictEqual(
-				tagName, moduleHelper.COMPONENT_PREFIX.toUpperCase() + 'SOME'
+				tagName, `${moduleHelper.COMPONENT_PREFIX.toUpperCase()}SOME`
 			);
 		});
-		it('should return name without prefix for HEAD', function() {
-			var tagName = moduleHelper.getTagNameForComponentName(
-				'head'
-			);
+
+		it('should return a name without the prefix for HEAD', function() {
+			const tagName = moduleHelper.getTagNameForComponentName('head');
 			assert.strictEqual(tagName, 'HEAD');
 		});
-		it('should return name HTML without prefix for document', function() {
-			var tagName = moduleHelper.getTagNameForComponentName(
-				'document'
-			);
+
+		it('should return a name "HTML" without the prefix for document', function() {
+			const tagName = moduleHelper.getTagNameForComponentName('document');
 			assert.strictEqual(
 				tagName, moduleHelper.DOCUMENT_ELEMENT_NAME.toUpperCase()
 			);
 		});
-		it('should return empty string for null value', function() {
-			var tagName = moduleHelper.getTagNameForComponentName(null);
+
+		it('should return an empty string for the null value', function() {
+			const tagName = moduleHelper.getTagNameForComponentName(null);
 			assert.strictEqual(tagName, '');
 		});
 	});
 
 	describe('#getMethodToInvoke', function() {
-		it('should find method in module', function() {
-			var module = {
-					someMethodToInvoke: function() {
-						return 'hello';
-					}
-				},
-				name = 'method-to-invoke',
-				method = moduleHelper.getMethodToInvoke(module, 'some', name);
+		it('should find a method in the module', function() {
+			const module = {
+				someMethodToInvoke: () => 'hello'
+			};
+			const name = 'method-to-invoke';
+			const method = moduleHelper.getMethodToInvoke(module, 'some', name);
 
 			assert.strictEqual(typeof (method), 'function');
 			assert.strictEqual(method(), 'hello');
 		});
 
-		it('should find default method in module and pass name into it',
-			function() {
-				var name = 'method-to-invoke',
-					module = {
-						some: function(passedName) {
-							assert.strictEqual(passedName, name);
-							return 'hello';
-						}
-					},
-					method = moduleHelper.getMethodToInvoke(
-						module, 'some', name
-					);
+		it('should find a default method in the module and pass the name into it', function() {
+			const name = 'method-to-invoke';
+			const module = {
+				some: passedName => {
+					assert.strictEqual(passedName, name);
+					return 'hello';
+				}
+			};
+			const method = moduleHelper.getMethodToInvoke(module, 'some', name);
 
-				assert.strictEqual(typeof (method), 'function');
-				assert.strictEqual(method(), 'hello');
-			});
+			assert.strictEqual(typeof (method), 'function');
+			assert.strictEqual(method(), 'hello');
+		});
 
-		it('should return method with promise if do not find in module',
-			function() {
-				var module = {
-					},
-					name = 'method-to-invoke',
-					method = moduleHelper.getMethodToInvoke(
-						module, 'some', name
-					);
+		it('should return a method with a promise if the method is not found in the module', function() {
+			const module = {};
+			const name = 'method-to-invoke';
+			const method = moduleHelper.getMethodToInvoke(module, 'some', name);
 
-				assert.strictEqual(typeof (method), 'function');
-				assert.strictEqual(method() instanceof Promise, true);
-			});
+			assert.strictEqual(typeof (method), 'function');
+			assert.strictEqual(method() instanceof Promise, true);
+		});
 
-		it('should return method with promise if arguments are wrong',
-			function() {
-				var module = null,
-					name = '',
-					method = moduleHelper.getMethodToInvoke(
-						module, 'some', name
-					);
+		it('should return a method with a promise if arguments are wrong', function() {
+			const module = null;
+			const name = '';
+			const method = moduleHelper.getMethodToInvoke(module, 'some', name);
 
-				assert.strictEqual(typeof (method), 'function');
-				assert.strictEqual(method() instanceof Promise, true);
-			});
+			assert.strictEqual(typeof (method), 'function');
+			assert.strictEqual(method() instanceof Promise, true);
+		});
 	});
 });
