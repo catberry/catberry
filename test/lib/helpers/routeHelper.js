54b1304556301469269d2ce115c5c1f2e234e19b
Denis Rechkunov <denis.rechkunov@gmail.com>
2016-02-20 20:42:34 +0100

    Migrate lib/helpers to ES2015.

--- test/lib/helpers/routeHelper.js
+++ test/lib/helpers/routeHelper.js
@@ -1,44 +1,17 @@
-/*
- * catberry
- *
- * Copyright (c) 2014 Denis Rechkunov and project contributors.
- *
- * catberry's license follows:
- *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without restriction,
- * including without limitation the rights to use, copy, modify, merge,
- * publish, distribute, sublicense, and/or sell copies of the Software,
- * and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- * This license applies to all parts of catberry that are not externally
- * maintained libraries.
- */
-
 'use strict';
 
-var assert = require('assert'),
-	testCases = require('../../cases/lib/helpers/routeHelper.json'),
-	routeHelper = require('../../../lib/helpers/routeHelper');
+const assert = require('assert');
+const testCases = require('../../cases/lib/helpers/routeHelper.json');
+const routeHelper = require('../../../lib/helpers/routeHelper');
 
+/* eslint prefer-arrow-callback:0 */
+/* eslint max-nested-callbacks:0 */
+/* eslint require-jsdoc:0 */
 describe('lib/helpers/routeHelper', function() {
 	describe('#removeEndSlash', function() {
-		testCases.removeEndSlash.forEach(function(testCase) {
+		testCases.removeEndSlash.forEach(testCase => {
 			it(testCase.name, function() {
-				var result = routeHelper.removeEndSlash(testCase.uri);
+				const result = routeHelper.removeEndSlash(testCase.uri);
 				assert.strictEqual(result, testCase.expected);
 			});
 		});
