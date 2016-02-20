9ae7bdef5f762a8a2e32ca28d637518fa257f65c
Denis Rechkunov <mail@pragmader.me>
2016-02-20 16:23:18 +0100

    Finish working on lib/ComponentFinder.

--- test/lib/finders/ComponentFinder.js
+++ test/lib/finders/ComponentFinder.js
@@ -1,146 +1,76 @@
-/*
- * catberry
- *
- * Copyright (c) 2014 Denis Rechkunov and project contributors.
- *
- * catberry's license follows:
- *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without restriction,
- * including without limitation the rights to use, copy, modify, merge,
- * publish, distribute, sublicense, and/or sell copies of the Software,
- * and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- * This license applies to all parts of catberry that are not externally
- * maintained libraries.
- */
-
 'use strict';
 
-var assert = require('assert'),
-	fs = require('fs'),
-	path = require('path'),
-	events = require('events'),
-	ServiceLocator = require('catberry-locator'),
-	Logger = require('../../mocks/Logger'),
-	ComponentFinder = require('../../../lib/finders/ComponentFinder');
+const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
+const events = require('events');
+const ServiceLocator = require('catberry-locator');
+const Logger = require('../../mocks/Logger');
+const ComponentFinder = require('../../../lib/finders/ComponentFinder');
 
-var CASE_PATH = path.join(
+const CASE_PATH = path.join(
 	'test', 'cases', 'lib', 'finders', 'ComponentFinder'
 );
 
-describe('lib/finders/ComponentFinder', function() {
+const EXPECTED_PATH = path.join(
+	process.cwd(), CASE_PATH, 'EXPECTED.json'
+);
+const EXPECTED = require(EXPECTED_PATH);
+
+/* eslint prefer-arrow-callback:0 */
+/* eslint max-nested-callbacks:0 */
+/* eslint require-jsdoc:0 */
+describe.only('lib/finders/ComponentFinder', function() {
+	var locator;
+
+	beforeEach(function() {
+		locator = new ServiceLocator();
+		locator.registerInstance('serviceLocator', locator);
+		locator.registerInstance('eventBus', new events.EventEmitter());
+		locator.register('componentFinder', ComponentFinder);
+		locator.register('logger', Logger);
+	});
+
 	describe('#find', function() {
 		it('should find all valid components', function(done) {
-			var locator = createLocator({
-					componentsGlob: 'test/**/test-cat-component.json'
-				}),
-				finder = locator.resolve('componentFinder');
-
-			var expectedPath = path.join(
-					process.cwd(), CASE_PATH, 'expected.json'
-				),
-				expected = require(expectedPath);
-
-			finder
+			locator.registerInstance('config', {
+				componentsGlob: 'test/**/test-cat-component.json'
+			});
+			const finder = locator.resolve('componentFinder');
+			return finder
 				.find()
-				.then(function(found) {
-					assert.strictEqual(
-						Object.keys(found).length,
-						Object.keys(expected).length,
-						'Wrong store count'
-					);
-
-					Object.keys(expected)
-						.forEach(function(name) {
-							assert.strictEqual(
-								(name in found), true,
-								name + ' not found'
-							);
-							assert.strictEqual(
-								found[name].name, expected[name].name
-							);
-							assert.strictEqual(
-								found[name].path, expected[name].path
-							);
-							assert.deepEqual(
-								found[name].properties,
-								expected[name].properties
-							);
-						});
-					done();
-				})
+				.then(found => foundEqualsExpected(found, EXPECTED))
+				.then(done)
 				.catch(done);
 		});
 		it('should find all valid components by globs array', function(done) {
-			var caseRoot = 'test/cases/lib/finders/ComponentFinder/components',
-				locator = createLocator({
-					componentsGlob: [
-						caseRoot + '/test1/**/test-cat-component.json',
-						caseRoot + '/test1/test-cat-component.json',
-						caseRoot + '/test3/**/test-cat-component.json',
-						caseRoot + '/test3/test-cat-component.json'
-					]
-				}),
-				finder = locator.resolve('componentFinder');
-
-			var expectedPath = path.join(
-					process.cwd(), CASE_PATH, 'expected.json'
-				),
-				expected = require(expectedPath);
-
-			finder
+			const caseRoot = 'test/cases/lib/finders/ComponentFinder/components';
+			locator.registerInstance('config', {
+				componentsGlob: [
+					caseRoot + '/test1/**/test-cat-component.json',
+					caseRoot + '/test1/test-cat-component.json',
+					caseRoot + '/test3/**/test-cat-component.json',
+					caseRoot + '/test3/test-cat-component.json'
+				]
+			});
+			const finder = locator.resolve('componentFinder');
+			return finder
 				.find()
-				.then(function(found) {
-					assert.strictEqual(
-						Object.keys(found).length,
-						Object.keys(expected).length,
-						'Wrong store count'
-					);
-
-					Object.keys(expected)
-						.forEach(function(name) {
-							assert.strictEqual(
-								(name in found), true,
-								name + ' not found'
-							);
-							assert.strictEqual(
-								found[name].name, expected[name].name
-							);
-							assert.strictEqual(
-								found[name].path, expected[name].path
-							);
-							assert.deepEqual(
-								found[name].properties,
-								expected[name].properties
-							);
-						});
-					done();
-				})
+				.then(found => foundEqualsExpected(found, EXPECTED))
+				.then(done)
 				.catch(done);
 		});
 	});
 });
 
-function createLocator(config) {
-	var locator = new ServiceLocator();
-	locator.registerInstance('serviceLocator', locator);
-	locator.registerInstance('config', config);
-	locator.registerInstance('eventBus', new events.EventEmitter());
-	locator.register('componentFinder', ComponentFinder, config);
-	locator.register('logger', Logger, config);
-	return locator;
+function foundEqualsExpected(found, EXPECTED) {
+	assert.strictEqual(found.size, Object.keys(EXPECTED).length);
+
+	Object.keys(EXPECTED)
+		.forEach(name => {
+			assert.strictEqual(found.has(name), true);
+			assert.strictEqual(found.get(name).name, EXPECTED[name].name);
+			assert.strictEqual(found.get(name).path, EXPECTED[name].path);
+			assert.deepEqual(found.get(name).properties, EXPECTED[name].properties);
+		});
 }
