a4e0418beb02e5f436a4ac95ffd13918ad2b19b3
Denis Rechkunov <mail@pragmader.me>
2016-02-21 16:25:08 +0100

    Migrate lib/providers to ES2015.

--- test/lib/providers/ModuleApiProvider.js
+++ test/lib/providers/ModuleApiProvider.js
@@ -1,97 +1,64 @@
-/*
- * catberry
- *
- * Copyright (c) 2014 Denis Rechkunov and project contributors.
- *
- * catberry's license follows:
- *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without restriction,
- * including without limitation the rights to use, copy, modify, merge,
- * publish, distribute, sublicense, and/or sell copies of the Software,
- * and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- * This license applies to all parts of catberry that are not externally
- * maintained libraries.
- */
-
 'use strict';
 
-var assert = require('assert'),
-	events = require('events'),
-	ServiceLocator = require('catberry-locator'),
-	CookieWrapper = require('../../../lib/CookieWrapper'),
-	ModuleApiProvider = require('../../../lib/providers/ModuleApiProvider');
+const assert = require('assert');
+const events = require('events');
+const ServiceLocator = require('catberry-locator');
+const CookieWrapper = require('../../../lib/CookieWrapper');
+const ModuleApiProvider = require('../../../lib/providers/ModuleApiProvider');
 
-global.Promise = require('promise');
+const TYPE_CHECK_METHOD_NAMES = [
+	'on',
+	'once',
+	'removeListener'
+];
 
+/* eslint prefer-arrow-callback:0 */
+/* eslint max-nested-callbacks:0 */
+/* eslint require-jsdoc:0 */
 describe('lib/providers/ModuleApiProvider', function() {
+	var locator, api;
+
+	beforeEach(function() {
+		locator = new ServiceLocator();
+
+		locator.register('cookieWrapper', CookieWrapper);
+		locator.registerInstance('serviceLocator', locator);
+		locator.registerInstance('eventBus', new events.EventEmitter());
+		api = new ModuleApiProvider(locator);
+	});
+
+	describe('Proper type checks and exceptions', function() {
+		TYPE_CHECK_METHOD_NAMES.forEach(methodName => {
+			describe(`#${methodName}`, function() {
+				it('should throw an error if the handler is not a function', function() {
+					assert.throws(() => api[methodName]('some', {}));
+				});
+
+				it('should throw an error if the event name is not a string', function() {
+					assert.throws(() => api[methodName]({}, function() {}));
+				});
+			});
+		});
+	});
+
 	describe('#on', function() {
-		it('should throw error if handler is not a function', function() {
-			var locator = createLocator(),
-				api = locator.resolveInstance(ModuleApiProvider);
+		it('should properly register a handler on the event', function(done) {
+			const bus = locator.resolve('eventBus');
 
-			assert.throws(function() {
-				api.on('some', {});
-			}, Error);
-		});
-		it('should throw error if event name is not a string', function() {
-			var locator = createLocator(),
-				api = locator.resolveInstance(ModuleApiProvider);
-
-			assert.throws(function() {
-				api.on({}, function() {});
-			}, Error);
-		});
-		it('should properly register handler on event', function(done) {
-			var locator = createLocator(),
-				bus = locator.resolve('eventBus'),
-				api = locator.resolveInstance(ModuleApiProvider);
-
-			api.on('event', function(arg) {
+			api.on('event', arg => {
 				assert.strictEqual(arg, 'hello');
 				done();
 			});
 			bus.emit('event', 'hello');
 		});
 	});
+
 	describe('#once', function() {
-		it('should throw error if handler is not a function', function() {
-			var locator = createLocator(),
-				api = locator.resolveInstance(ModuleApiProvider);
-
-			assert.throws(function() {
-				api.once('some', {});
-			}, Error);
-		});
-		it('should throw error if event name is not a string', function() {
-			var locator = createLocator(),
-				api = locator.resolveInstance(ModuleApiProvider);
-
-			assert.throws(function() {
-				api.once({}, function() {});
-			}, Error);
-		});
-		it('should properly register handler on event', function(done) {
-			var locator = createLocator(),
-				bus = locator.resolve('eventBus'),
-				api = locator.resolveInstance(ModuleApiProvider);
+		it('should properly register a handler on the event', function(done) {
+			const bus = locator.resolve('eventBus');
 
 			var was = false;
-			api.once('event', function(arg) {
+			api.once('event', arg => {
 				if (was) {
 					assert.fail();
 				}
@@ -103,32 +70,13 @@ describe('lib/providers/ModuleApiProvider', function() {
 			done();
 		});
 	});
+
 	describe('#removeListener', function() {
-		it('should throw error if handler is not a function', function() {
-			var locator = createLocator(),
-				api = locator.resolveInstance(ModuleApiProvider);
+		it('should properly remove a listener', function(done) {
+			const bus = locator.resolve('eventBus');
 
-			assert.throws(function() {
-				api.removeListener('some', {});
-			}, Error);
-		});
-		it('should throw error if event name is not a string', function() {
-			var locator = createLocator(),
-				api = locator.resolveInstance(ModuleApiProvider);
-
-			assert.throws(function() {
-				api.removeListener({}, function() {});
-			}, Error);
-		});
-		it('should properly remove listener', function(done) {
-			var locator = createLocator(),
-				bus = locator.resolve('eventBus'),
-				api = locator.resolveInstance(ModuleApiProvider);
-
-			var was = false,
-				handler = function() {
-					was = true;
-				};
+			var was = false;
+			const handler = () => (was = true);
 
 			api.on('event', handler);
 			api.removeListener('event', handler);
@@ -137,27 +85,18 @@ describe('lib/providers/ModuleApiProvider', function() {
 			done();
 		});
 	});
+
 	describe('#removeAllListeners', function() {
-		it('should throw error if event name is not a string', function() {
-			var locator = createLocator(),
-				api = locator.resolveInstance(ModuleApiProvider);
-
-			assert.throws(function() {
-				api.removeAllListeners({});
-			}, Error);
+		it('should throw an error if the event name is not a string', function() {
+			assert.throws(() => api.removeAllListeners({}));
 		});
-		it('should properly remove all listeners', function(done) {
-			var locator = createLocator(),
-				bus = locator.resolve('eventBus'),
-				api = locator.resolveInstance(ModuleApiProvider);
 
-			var was = false,
-				handler1 = function() {
-					was = true;
-				},
-				handler2 = function() {
-					was = true;
-				};
+		it('should properly remove all listeners', function(done) {
+			const bus = locator.resolve('eventBus');
+
+			var was = false;
+			const handler1 = () => (was = true);
+			const handler2 = () => (was = true);
 
 			api.on('event', handler1);
 			api.on('event', handler2);
@@ -167,38 +106,43 @@ describe('lib/providers/ModuleApiProvider', function() {
 			done();
 		});
 	});
-	describe('#redirect', function() {
-		it('should save last redirected URI', function() {
-			var locator = createLocator(),
-				api = locator.resolveInstance(ModuleApiProvider);
-			assert.strictEqual(api.redirect('/some1') instanceof Promise, true);
-			assert.strictEqual(api.redirect('/some2') instanceof Promise, true);
-			assert.strictEqual(api.actions.redirectedTo, '/some2');
+
+	describe('#redirect', function(done) {
+		it('should save the last redirected URI', function() {
+			api.redirect('/some1')
+				.then(() => api.redirect('/some2'))
+				.then(() => assert.strictEqual(api.actions.redirectedTo, '/some2'))
+				.then(done)
+				.catch(done);
 		});
 	});
-	describe('#clearFragment', function() {
+
+	describe('#clearFragment', function(done) {
 		it('should save flag that hash has been cleared', function() {
-			var locator = createLocator(),
-				api = locator.resolveInstance(ModuleApiProvider);
 			assert.strictEqual(api.actions.isFragmentCleared, false);
-			assert.strictEqual(api.clearFragment() instanceof Promise, true);
-			assert.strictEqual(api.actions.isFragmentCleared, true);
+			api.clearFragment()
+				.then(() => assert.strictEqual(api.actions.isFragmentCleared, true))
+				.then(done)
+				.catch(done);
 		});
 	});
-	describe('#getInlineScript', function() {
+
+	describe('#getInlineScript', function(done) {
 		it('should return browser script for redirection', function(done) {
-			var locator = createLocator(),
-				api = locator.resolveInstance(ModuleApiProvider);
-			api.redirect('http://some');
-			var expected = '<script>' +
-				'window.location.assign(\'http://some\');' +
-				'</script>';
-			assert.strictEqual(api.getInlineScript(), expected);
-			done();
+			const expected = '<script>window.location.assign(\'http://some\');</script>';
+
+			api.redirect('http://some')
+				.then(() => assert.strictEqual(api.getInlineScript(), expected))
+				.then(done)
+				.catch(done);
 		});
+
 		it('should return browser script for cookies', function(done) {
-			var locator = createLocator(),
-				api = locator.resolveInstance(ModuleApiProvider);
+			const expected = '<script>' +
+				'window.document.cookie = \'some1=value1\';' +
+				'window.document.cookie = \'some2=value2\';' +
+				'</script>';
+
 			api.cookie.set({
 				key: 'some1',
 				value: 'value1'
@@ -207,33 +151,20 @@ describe('lib/providers/ModuleApiProvider', function() {
 				key: 'some2',
 				value: 'value2'
 			});
-			var expected = '<script>' +
-				'window.document.cookie = \'some1=value1\';' +
-				'window.document.cookie = \'some2=value2\';' +
-				'</script>';
+
 			assert.strictEqual(api.getInlineScript(), expected);
 			done();
 		});
-		it('should return browser script ' +
-		'for clearing fragment', function(done) {
-			var locator = createLocator(),
-				api = locator.resolveInstance(ModuleApiProvider);
-			api.clearFragment();
-			var expected = '<script>' +
+
+		it('should return browser script for clearing fragment', function(done) {
+			const expected = '<script>' +
 				'window.location.hash = \'\';' +
 				'</script>';
-			assert.strictEqual(api.getInlineScript(), expected);
-			done();
+
+			api.clearFragment()
+				.then(() => assert.strictEqual(api.getInlineScript(), expected))
+				.then(done)
+				.catch(done);
 		});
 	});
 });
-
-function createLocator() {
-	var locator = new ServiceLocator();
-
-	locator.register('cookieWrapper', CookieWrapper);
-	locator.registerInstance('serviceLocator', locator);
-	locator.registerInstance('eventBus', new events.EventEmitter());
-
-	return locator;
-}
